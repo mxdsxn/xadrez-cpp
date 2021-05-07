32ce6031730bcf904756523af5b09bc2a476614f
Madson Santana <madson.santana@dextra-sw.com>
2021-05-07 20:30:49 -0300

    Main: inicia jogo

--- chess-test/main.cpp
+++ chess-test/main.cpp
@@ -1,114 +1,14 @@
-#include "./Tabuleiro.h"
-#include "./Posicao.h"
-
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-struct CoordenadasStr
-{
-  string x, y;
-};
-
-/**
- * Coverte coordenadas de uma posição para ser mostrada no console
- * @param posicao Posição que precisa ser formatada as coordenadas
- * @return Retorna uma lista de tamanho 2. Primeiro item X, segundo item, Y.
- * | Lista vazia caso as coordenadas não atendam a regra.
- */
-CoordenadasStr posicaoToCoordStr(Posicao &posicao)
-{
-  CoordenadasStr coordenadasFormatadas;
-
-  int x = posicao.getX();
-  int y = posicao.getY();
-
-  if ((x >= 0 && x < 8) && (y >= 0 && y < 8))
-  {
-    coordenadasFormatadas.y = to_string(y + 1);
-    coordenadasFormatadas.x = (char)(x + 65);
-  }
-
-  return coordenadasFormatadas;
-}
-
-/**
- * Formata coordenadas para o formato [A, 1]
- * @param coordenadas Coordenadas para serem formatadas.
- * @return Retorna uma string no formato [A, 1].
- */
-string formataCoordenadas(CoordenadasStr coordenadas)
-{
-  return "[" + coordenadas.x + ", " + coordenadas.y + "]";
-}
+#include "./Principal.h"
+#include "./clear.h"
 
 int main()
 {
-  int jogadorSelecionado = 1;
 
-  Tabuleiro *tabuleiro = new Tabuleiro();
+  clear();
 
-  tabuleiro->show(1);
+  Principal *novoJogo = new Principal();
 
-  // procura peças disponiveis de um jogador
-  vector<Posicao *> posicoesPecasDisponiveisJogador1 = tabuleiro->getPosicaoPecasDisponiveis(jogadorSelecionado);
-
-  // mostra no console as peças e suas coordenadas para o jogador escolher a peça
-  cout << "Pecas disponiveis: " << endl
-       << endl;
-
-  for (int i = 0; i < posicoesPecasDisponiveisJogador1.size(); i++)
-  {
-    Posicao *posicaoAtual = posicoesPecasDisponiveisJogador1[i];
-    CoordenadasStr coordenadas = posicaoToCoordStr(*posicaoAtual);
-    cout << " - " << posicaoAtual->showSimbolo() << " | " << formataCoordenadas(coordenadas) << endl;
-  }
-
-  Posicao *posi = posicoesPecasDisponiveisJogador1[1];
-  Peca *peca = posi->getPecaAtual();
-  vector<Posicao *> posicoesDisponiveisPeca = peca->getPosicoesValidas(tabuleiro->getTodasPosicoes());
-
-  tabuleiro->show(jogadorSelecionado);
-
-  // mostra no console as peças e suas coordenadas para o jogador escolher a peça
-  cout << "Posicoes disponiveis: " << endl
-       << formataCoordenadas(posicaoToCoordStr(*(peca->getPosicaoAtual())))
-       << endl;
-  for (int i = 0; i < posicoesDisponiveisPeca.size(); i++)
-  {
-    Posicao *posicaoAtual = posicoesDisponiveisPeca[i];
-    CoordenadasStr coordenadas = posicaoToCoordStr(*posicaoAtual);
-    cout << " - " << posicaoAtual->showSimbolo() << " | " << formataCoordenadas(coordenadas) << endl;
-  }
-
-  // move a peca para a posicao desejada
-  for (int i = 0; i < posicoesDisponiveisPeca.size(); i++)
-  {
-    Posicao *posicaoAtual = posicoesDisponiveisPeca[i];
-    if (posicaoAtual->getX() == 1 && posicaoAtual->getY() == 2)
-    {
-      peca->movimentar(posicaoAtual);
-    }
-  }
-
-  tabuleiro->show(jogadorSelecionado);
-
-  peca = posicoesPecasDisponiveisJogador1[0]->getPecaAtual();
-  posicoesPecasDisponiveisJogador1 = tabuleiro->getPosicaoPecasDisponiveis(1);
-  posicoesDisponiveisPeca = peca->getPosicoesValidas(tabuleiro->getTodasPosicoes());
-  // mostra no console as peças e suas coordenadas para o jogador escolher a peça
-  cout << "Posicoes disponiveis: " << endl
-       << formataCoordenadas(posicaoToCoordStr(*(peca->getPosicaoAtual())))
-       << endl;
-  for (int i = 0; i < posicoesDisponiveisPeca.size(); i++)
-  {
-    Posicao *posicaoAtual = posicoesDisponiveisPeca[i];
-    CoordenadasStr coordenadas = posicaoToCoordStr(*posicaoAtual);
-    cout << " - " << posicaoAtual->showSimbolo() << " | " << formataCoordenadas(coordenadas) << endl;
-  }
-
-  cout << "Rei está em cheque: " << (peca->verificaXequeAdversario(posicoesDisponiveisPeca[0], tabuleiro->getTodasPosicoes()) ? "sim" : "nao") << endl;
+  novoJogo->iniciar();
 
   return 0;
 }
