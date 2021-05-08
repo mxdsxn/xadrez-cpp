86404baa2d2e85dc06afa8c7e01d10dc0562b9a5
Madson Santana <madson.santana@dextra-sw.com>
2021-05-08 11:20:16 -0300

    PartidaTradicional: descrevendo metodos

--- chess-test/PartidaTradicional.h
+++ chess-test/PartidaTradicional.h
@@ -10,11 +10,25 @@ private:
   Jogador *segundoJogador;
   bool turnoPrimeiroJogador;
 
+  /**
+    * Mostra ao usuario o tabuleiro ao usuario
+    * @param turnoPrimeiroJogador Booleano que quando TRUE, mostra o tabuleiro orientado para o primeiroJogador, FALSE, orientado ao segundoJogador
+  */
   void mostrarTabuleiro(bool turnoPrimeiroJogador);
+
+  /**
+    * Mostra ao usuario as peças disponiveis no turno atual
+    * @param primeiroJogador Booleano que quando TRUE, corresponde ao primeiroJogador, FALSE, ao segundoJogador
+    * @return Peça selecionada para a jogada no turno atual
+  */
   Peca *selecionarPecaDisponivel(bool primeiroJogador);
 
 public:
   PartidaTradicional(string nomePrimeiroJogador, string nomeSegundoJogador);
+
+  /**
+    * Inicia o jogo - gerencia os turnos da partida
+  */
   void iniciarPartida();
   void salvarPartida();
   void encerrarPartida();
